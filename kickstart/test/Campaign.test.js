2492157c966f063095d35bf3fd97f25daadf443f
sikezero <imiller@protonmail.com>
2018-04-04 20:39:40 -0400

    new

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -1,4 +1,4 @@
-require('events').EventEmitter.defaultMaxListeners = 25;
+require('events').EventEmitter.defaultMaxListeners = 50;
 
 const assert = require('assert');
 const ganache = require('ganache-cli');
@@ -53,6 +53,53 @@ describe('Campaigns', () => {
     });
     const isContributor = await campaign.methods.approvers(accounts[1]).call();
     assert(isContributor);
+  });
+
+  it('requires a minimum contribution', async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: '5',
+        from: accounts[1]
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods.createRequest('Buy batteries', '100', accounts[1]).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+    const request = await campaign.methods.requests(0).call();
+    assert.equal('Buy batteries', request.description);
+  });
+
+  it('processes requests', async () => {
+    const initialBalance = parseFloat(web3.utils.fromWei(await web3.eth.getBalance(accounts[1]), 'ether'));
+    
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether')
+    });
+
+    await campaign.methods.createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({ from: accounts[0], gas: '1000000' });
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, 'ether')
+    balance = parseFloat(balance);
+    assert(balance > initialBalance + 4.9);
   });
 
 });
